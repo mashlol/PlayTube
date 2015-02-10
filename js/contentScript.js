aefed92e00bc39b53105587e94e0f3f3a2e0161a
Kevin Bedi <kevin@kevinbedi.com>
2015-02-09 21:42:12 -0800

    Downscale a bit on the resolution

--- js/contentScript.js
+++ js/contentScript.js
@@ -52,8 +52,8 @@ var animFrame = function() {
   ctx.drawImage(document.getElementsByClassName("video-stream")[0],
     0,
     0,
-    800,
-    400
+    400,
+    200
   );
 
   chrome.runtime.sendMessage({
@@ -125,8 +125,8 @@ chrome.runtime.sendMessage({action: "isPlayTab"}, function(response) {
     playButton = document.getElementsByClassName("ytp-button-pause")[0];
 
     canvas = document.createElement("canvas");
-    canvas.width = 800;
-    canvas.height = 400;
+    canvas.width = 400;
+    canvas.height = 200;
     ctx = canvas.getContext('2d');
   }
 });
